43b48e397ed157d1fe5b3c6e0dc02401c5c2f696
juanmanoel237 <ndeguemanoeljuan@gmail.com>
2024-06-14 10:26:53 +0200

    Mise en place du serveur et de la base de données. Création des deux models User et Book

---
+++ Backend/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Charger les variables d'environnement à partir de .env
+dotenv.config();
+// Utiliser la variable d'environnement pour la connexion MongoDB
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((err) => console.log('Connexion à MongoDB échouée !', err));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json())
+
+module.exports = app;
